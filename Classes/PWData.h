ee8b21d5dbca7841b06049a565adc6a41e96c03a
Andy Sawyer <git@pureabstract.org>
2012-05-31 20:15:08 +0100

    Added PWData class

---
+++ Classes/PWData.h
@@ -0,0 +1,16 @@
+//
+//  PWData.h
+//  PWStore
+//
+//  Created by Andy Sawyer on 31/05/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PWData : NSObject {
+
+}
+
+@end
---
+++ Classes/PWData.m
@@ -0,0 +1,14 @@
+//
+//  PWData.m
+//  PWStore
+//
+//  Created by Andy Sawyer on 31/05/2012.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PWData.h"
+
+
+@implementation PWData
+
+@end
