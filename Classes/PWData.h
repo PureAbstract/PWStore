0c3d79467a9f6c91a164f32b1ab762e9f6aab4b1
Andy Sawyer <git@pureabstract.org>
2012-05-31 21:28:41 +0100

    Update copyright message

--- Classes/PWData.h
+++ Classes/PWData.h
@@ -3,7 +3,7 @@
 //  PWStore
 //
 //  Created by Andy Sawyer on 31/05/2012.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//  Copyright 2012 Andy Sawyer. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
