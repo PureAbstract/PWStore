888fa2acd9f2145fa394a7b65f6b30efb28f31fd
Andy Sawyer <git@pureabstract.org>
2012-06-01 19:31:09 +0100

    Updated ItemEditViewController

--- Classes/ItemEditViewController.h
+++ Classes/ItemEditViewController.h
@@ -9,8 +9,21 @@
 #import <UIKit/UIKit.h>
 #import "PWItem.h"
 
-@interface ItemEditViewController : UIViewController {
+@interface ItemEditViewController : UIViewController<UITextFieldDelegate, UITextViewDelegate> {
     PWItem *item_;
+    IBOutlet UITextField *titleField;
+    IBOutlet UITextField *loginField;
+    IBOutlet UITextField *passwordField;
+    IBOutlet UITextField *urlField;
+    IBOutlet UITextField *emailField;
+    IBOutlet UITextView *notesField;
 }
 @property (nonatomic,retain) PWItem *item;
+@property (nonatomic,retain) IBOutlet UITextField *titleField;
+@property (nonatomic,retain) IBOutlet UITextField *loginField;
+@property (nonatomic,retain) IBOutlet UITextField *passwordField;
+@property (nonatomic,retain) IBOutlet UITextField *urlField;
+@property (nonatomic,retain) IBOutlet UITextField *emailField;
+@property (nonatomic,retain) IBOutlet UITextView *notesField;
+
 @end
--- Classes/ItemEditViewController.m
+++ Classes/ItemEditViewController.m
@@ -7,12 +7,19 @@
 //
 
 #import "ItemEditViewController.h"
-
+#import <QuartzCore/QuartzCore.h>
 
 @implementation ItemEditViewController
 #pragma mark -
 #pragma mark Properties
 @synthesize item = item_;
+@synthesize titleField;
+@synthesize loginField;
+@synthesize passwordField;
+@synthesize urlField;
+@synthesize emailField;
+@synthesize notesField;
+#pragma mark -
 
 // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
@@ -25,12 +32,25 @@
 }
 */
 
-/*
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if( item_ ) {
+        titleField.text = item_.title;
+        loginField.text = item_.login;
+        passwordField.text = item_.password;
+        urlField.text = item_.url;
+        emailField.text = item_.email;
+        notesField.text = item_.notes;
+
+    }
+    // Magic...
+    notesField.layer.borderWidth = 1;
+    notesField.layer.borderColor = [[UIColor grayColor] CGColor];
+    notesField.layer.cornerRadius = 5;
+
+    [titleField becomeFirstResponder];
 }
-*/
 
 /*
 // Override to allow orientations other than the default portrait orientation.
@@ -51,12 +71,40 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.titleField = nil;
+    self.loginField = nil;
+    self.passwordField = nil;
+    self.urlField = nil;
+    self.emailField = nil;
+    self.notesField = nil;
 }
 
 
 - (void)dealloc {
+    [self.titleField release];
+    [self.loginField release];
+    [self.passwordField release];
+    [self.urlField release];
+    [self.emailField release];
+    [self.notesField release];
     [item_ release];
     [super dealloc];
+}
+
+#pragma mark -
+#pragma mark UITextFieldDelegate
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    NSInteger tag = textField.tag;
+    if( tag ) {
+        NSInteger tagNext = (tag+1);
+        UIView *next = [self.view viewWithTag:tagNext];
+        if( next ) {
+            [next becomeFirstResponder];
+        }
+    }
+    return YES;
 }
 
 
