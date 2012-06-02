48361467d9f3f69388b37e1b4761de8d2443f5c7
Andy Sawyer <git@pureabstract.org>
2012-06-02 21:58:07 +0100

    Fixed copyright in NSMutableArray+Utility

--- Classes/NSMutableArray+Utility.m
+++ Classes/NSMutableArray+Utility.m
@@ -3,7 +3,7 @@
 //  PWStore
 //
 //  Created by Andy Sawyer on 02/06/2012.
-//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//  Copyright 2012 Andy Sawyer. All rights reserved.
 //
 
 #import "NSMutableArray+Utility.h"
